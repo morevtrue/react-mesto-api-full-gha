8d6dec11ef556fed941d43edc9cedf2d58fe2936
Vyacheslav Morev <gromd96@gmail.com>
2023-08-08 00:09:09 +0300

    поменять адрес порта подключения к апи

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -95,7 +95,7 @@ export default class Api {
 }
 
 export const api = new Api({
-  baseUrl: 'http://localhost:4000/',
+  baseUrl: 'https://api.mestomorev.students.nomoreparties.co/',
   headers: {
     // authorization: '3f6b2ef5-b9e5-4ebd-9f38-797a06c223a7',
     'Content-Type': 'application/json'
